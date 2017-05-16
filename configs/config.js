abf25a1987f84621d84d781e5809053229570b8d
Jean-Christophe Sirot <jsirot@weborama.com>
2017-05-16 19:17:36 +0200

    Initial commit

---
+++ configs/config.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+var config;
+
+config = {
+  production: {
+    url: 'http://ghost.example.com',
+    database: {
+      client: 'mysql',
+      connection: {
+        host: 'mysql',
+        user: 'ghost',
+        password: 'myp4ss',
+        database: 'ghost',
+        charset: 'utf8',
+      }
+    },
+    server: {
+      host: '127.0.0.1',
+      port: '2368'
+    },
+    paths: {
+      contentPath: '/var/lib/ghost'
+    },
+  }
+}
+
+module.exports = config;
